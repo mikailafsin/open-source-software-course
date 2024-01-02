32ab09db1a56a94a381dbc3e311e16aca976d505
mikailafsin <mikailwnchstr@gmail.com>
2024-01-02 10:45:38 +0000

    flask-on-docker has been created.

---
+++ flask-on-docker/README.md
@@ -0,0 +1,34 @@
+# Flask ve Docker ile Örnek Uygulama
+
+Bu proje, Flask web uygulamasını Docker container'ında çalıştırmak için basit bir şablondur.
+
+## Gereksinimler
+
+- Docker
+
+## Nasıl Kullanılır
+
+1. Bu depoyu bilgisayarınıza klonlayın:
+
+    ```bash
+    git clone https://github.com/mikailafsin/open-source-software-course.git
+    cd flask-on-docker
+    ```
+
+2. Docker container'ını oluşturun ve çalıştırın:
+
+    ```bash
+    docker compose build
+    docker compose up -d
+    ```
+
+3. Tarayıcınızda `http://localhost:8000` adresine giderek uygulamayı görüntüleyin.
+
+## Proje Yapısı
+
+- `docker-compose.yml`: Docker container'ını başlatmak için kullanılır.
+- `README.md`: Bu dosya.
+- `main.py`: Flask uygulamasının kodlarını içerir.
+- `Dockerfile`: Docker container'ınızın nasıl oluşturulacağını tanımlar.
+- `requirements.txt`: Gerekli Python kütüphanelerini içerir.
+- `users.csv`: Kullanıcı bilgilerini depolar.
---
+++ flask-on-docker/project/main.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+import pandas as pd
+import requests
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+   def get(self,sayi):
+       url = "https://meowfacts.herokuapp.com/?count="+ sayi
+       response = requests.get(url)
+       data = response.json()
+       return {'data' : data['data']}, 200
+
+   def post(self):
+       name = request.args['name']
+       age = request.args['age']
+       city = request.args['city']
+
+       req_data = pd.DataFrame({
+           'name'      : [name],
+           'age'       : [age],
+           'city'      : [city]
+       })
+
+       data = pd.read_csv('users.csv')
+       data = data.append(req_data, ignore_index=True)
+       data.to_csv('users.csv', index=False)
+       return {'message' : 'Record successfully added.'}, 201
+
+class Name(Resource):
+   def get(self,name):
+       data = pd.read_csv('users.csv')
+       data = data.to_dict('records')
+       for entry in data:
+           if entry['name'] == name :
+               return {'data' : entry}, 200
+       return {'message' : 'No entry found with this name !'}, 400
+
+api.add_resource(Users, '/users/<string:sayi>')
+api.add_resource(Name, '/isim/<string:name>')
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=80)
+   app.run()
